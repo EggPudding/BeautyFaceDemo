d15e4a2e09fa560365b072d36c2a1be66d90e5fd
EggPudding <ksj970531@gmail.com>
2021-08-07 17:24:14 +0900

    Decorate web page with sample

--- src/App.js
+++ src/App.js
@@ -1,63 +1,18 @@
-// import "./App.css";
+import "./App.css";
 import React, { Component, useState } from "react";
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ol>
-          <li>
-            <a
-              href="1.html"
-              onClick={function (ev) {
-                ev.preventDefault();
-              }}
-            >
-              HTML
-            </a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-        </ol>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h3>world</h3>
-      </article>
-    );
-  }
-}
+import sample from "./asset/sample.jpeg";
 
 function App() {
-  const [content, setContent] = useState([
-    { id: 1, title: "TITLE", desc: "TITLE dectiption" },
-    { id: 2, title: "TITLE2", desc: "TITLE dectiption" },
-    { id: 3, title: "TITLE3", desc: "TITLE dectiption" },
-  ]);
+  console.log(require("./asset/sample.jpeg"));
 
   return (
     <div className="App">
-      <Subject title="Title" sub="Hello, world!"></Subject>
-      <TOC data={content}></TOC>
-      <Content></Content>
+      <img src={sample} aria-hidden alt="asset/sample.jpeg Image" />
+      <header className="Text">
+        <h1>BeautyFace Demo</h1>
+        <h2>Guideline..</h2>
+        {"<"} Load your image {">"}
+      </header>
     </div>
   );
 }
